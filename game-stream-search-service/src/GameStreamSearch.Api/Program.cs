969bd61a69e873e405697f8fc9c4dddf0732930f
Kerry Convery <kerry.convery@myob.com>
2020-11-03 22:54:35 +1100

    Moved api keys to parameter store and rotated them

--- game-stream-search-service/src/GameStreamSearch.Api/Program.cs
+++ game-stream-search-service/src/GameStreamSearch.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace GameStreamSearch.Api
@@ -12,6 +13,10 @@ namespace GameStreamSearch.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddSystemsManager("/stream-machine");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
