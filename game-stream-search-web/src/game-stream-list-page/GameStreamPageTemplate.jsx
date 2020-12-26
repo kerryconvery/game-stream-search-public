f60c814599a4b0267521be1ee6dcbf00ab9269a0
Kerry Convery <kerry.convery@myob.com>
2020-12-27 00:30:15 +1100

    Refactored to improve component cohesion

--- game-stream-search-web/src/game-stream-list-page/GameStreamPageTemplate.jsx
+++ game-stream-search-web/src/game-stream-list-page/GameStreamPageTemplate.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { node, bool, number } from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import StandardPageTemplate from '../templates/StandardPageTemplate';
+import StandardPageTemplate from '../components/StandardPageTemplate';
 
 const useStyles = makeStyles(() => ({
   gridContainer: {
